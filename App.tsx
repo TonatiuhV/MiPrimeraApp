01036a48de7d5fca225ca85319f69b838cd552da
= <=>
2022-05-13 19:53:03 -0500

    SafeAreaView

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import { ContadorScreen } from './src/screens/ContadorScreen';
+// Gaantiza que se muestre el contenido adecuadamente en todos os dispositivos
+import { SafeAreaView } from 'react-native';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+// import { ContadorScreen } from './src/screens/ContadorScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
+
 
 const App = () => {
   return (
-    // <HolaMundoScreen/> 
-    <ContadorScreen/>
+    <SafeAreaView>
+        {/* <HolaMundoScreen/> 
+        <ContadorScreen/> */}
+        <BoxObjectModelScreen/>
+    </SafeAreaView>
     )
 }
 export default App;
