c648357e688e8b858d442fe2168b85ebcb53d88c
= <=>
2022-05-17 22:17:16 -0500

    Tarea 6

--- App.tsx
+++ App.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 import { SafeAreaView } from 'react-native';
 // import { Tarea2Screen } from './src/screens/Tarea2Screen';
 // import { Tarea3Screen } from './src/screens/Tarea3Screen';
-import { Tarea4Screen } from './src/screens/Tarea4Screen';
-import { Tarea5Screen } from './src/screens/Tarea5Screen';
+// import { Tarea4Screen } from './src/screens/Tarea4Screen';
+// import { Tarea5Screen } from './src/screens/Tarea5Screen';
+import { Tarea6Screen } from './src/screens/Tarea6Screen';
 // import { Tarea1Screen } from './src/screens/Tarea1Screen';
 // import { FlexScreen } from './src/screens/FlexScreen';
 // import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
@@ -28,7 +29,8 @@ const App = () => {
         {/* <Tarea2Screen/> */}
         {/* <Tarea3Screen/> */}
         {/* <Tarea4Screen/> */}
-        <Tarea5Screen/>
+        {/* <Tarea5Screen/> */}
+        <Tarea6Screen/>
     </SafeAreaView>
     )
 }
